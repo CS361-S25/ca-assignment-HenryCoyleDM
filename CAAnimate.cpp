ab9ba4de3a7c5ecf53d25f327acfd10658bcc552
HenryCoyleDM <henry.coyle65536@gmail.com>
2025-04-14 08:36:16 -0500

    Implemented Game of Life

--- CAAnimate.cpp
+++ CAAnimate.cpp
@@ -14,7 +14,10 @@ const double height{num_h_boxes * RECT_SIDE};
 
 
 //some vectors to hold information about the CA
-std::vector<std::vector<int> > cells;
+std::vector<std::vector<int>> cells;
+
+// vector to temporarily store cells for next generation
+std::vector<std::vector<int>> next_generation;
         
 
 // where we'll draw
@@ -31,9 +34,9 @@ public:
 
         //fill the vectors with 0 to start
         cells.resize(num_w_boxes, std::vector<int>(num_h_boxes, 0));
+        next_generation.resize(num_w_boxes, std::vector<int>(num_h_boxes, 0));
 
-        //showing how to set a cell to 'alive'
-        cells[0][0] = 1;
+        AddGlider();
     }
 
     void DoFrame() override {
@@ -50,11 +53,78 @@ public:
                     canvas.Rect(x * RECT_SIDE, y * RECT_SIDE, RECT_SIDE, RECT_SIDE, "black", "black");
                 }
                 
+                
             }
         }
 
-        //Place to update the values of all the cells based on CA rules
+        // update all cell values using update rules
+        ComputeNextGeneration();
+        UpdateGridFromNextGeneration();
 
+    }
+
+    // get the value of the cell at (x, y), wrapping around if they are out of bounds
+    int GetCell(int x, int y) {
+        return cells[emp::Mod(x, num_w_boxes)][emp::Mod(y, num_h_boxes)];
+    }
+
+    // a cell is considered an alive neighbor if it differs in each coordinate from (x, y) by at most 1 and has value > 0
+    int NumberOfAliveNeighbors(int x, int y) {
+        int count = 0;
+        for (int ox = -1; ox <= 1; ox++) {
+            for (int oy = -1; oy <= 1; oy++) {
+                // don't count this cell
+                if ((ox != 0 || oy != 0) && GetCell(x + ox, y + oy) > 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    // apply the update rule to each cell to determine what it will be in the next generation
+    void ComputeNextGeneration() {
+        for (int x=0; x<num_w_boxes; x++) {
+            for (int y=0; y<num_h_boxes; y++) {
+                next_generation[x][y] = UpdateFunction(x, y);
+            }
+        }
+    }
+
+    // applies the newly calculated values to the cells in the grid to step to the next generation
+    void UpdateGridFromNextGeneration() {
+        for (int x=0; x<num_w_boxes; x++) {
+            for (int y=0; y<num_h_boxes; y++) {
+                cells[x][y] = next_generation[x][y];
+            }
+        }
+    }
+
+    // returns the state that the cell at (x, y) will change to
+    int UpdateFunction(int x, int y) {
+        return LifeUpdateFunction(x, y);
+    }
+
+    // the update function for Conway's Game of Life
+    int LifeUpdateFunction(int x, int y) {
+        int number_of_alive_neighbors = NumberOfAliveNeighbors(x, y);
+        bool is_alive = GetCell(x, y) > 0;
+        if (is_alive) {
+            // an alive cell survives if it has 2 or 3 neighbors
+            return (number_of_alive_neighbors == 2 || number_of_alive_neighbors == 3) ? 1 : 0;
+        } else {
+            // a dead cell is born if it has 3 neighbors
+            return number_of_alive_neighbors == 3 ? 1 : 0;
+        }
+    }
+
+    // adds a glider from Conway's Game of Life to the scene at (0, 0) facing southeast
+    void AddGlider() {
+        cells[1][0] = 1;
+        cells[2][1] = 1;
+        cells[0][2] = 1;
+        cells[1][2] = 1;
+        cells[2][2] = 1;
     }
 
 
